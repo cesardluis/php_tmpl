63cc0bccb28e9b6ba93ee6a4db013f071ee1c67c
Cesar Luis <cesardluis@gmail.com>
2013-08-01 13:10:08 -0300

    tab

--- tmpl.class.php
+++ tmpl.class.php
@@ -1,36 +1,35 @@
 <?php
 
 /**
- * @author cesardluis
- * @copyright 2013
- */
+* @author cesardluis
+* @copyright 2013
+*/
 
 class tmpl{
     private $template;
     /**
-     * [__construct description]
-     * @param [template path] $file
-     * @param array  $data [array associative with data]
-     */
+    * [__construct description]
+    * @param [template path] $file
+    * @param array  $data [array associative with data]
+    */
     public function __construct($file, $data=array()) {
         $this->inHtml($file, $data);
     }
 
     public function inHtml($file, $data){
-         if ($this->template = file_get_contents($file)) {
-         	if(count($data) > 0){
-				$vars = array();
-				$values = array();
-				foreach ($data as $key => $value) {
-					$vars[] = "{".$key."}";
-					$values[] = $value;
-				}
-				$this->template = str_replace ($vars, $values, $this->template);
-				$this->template = preg_replace('#\{([a-z0-9\-_]*?)\}#is', '', $this->template);
-			}            
-        }else{
+        if ($this->template = file_get_contents($file)) {
+            if(count($data) > 0){
+                $vars = array();
+                $values = array();
+                foreach ($data as $key => $value) {
+                    $vars[] = "{".$key."}";
+                    $values[] = $value;
+                }
+                $this->template = str_replace ($vars, $values, $this->template);
+                $this->template = preg_replace('#\{([a-z0-9\-_]*?)\}#is', '', $this->template);
+            }            
+        }else
             echo 'Error: '.$file;
-        }
     }
 
     public function display(){
@@ -38,7 +37,7 @@ class tmpl{
     }
 
     public function get(){
-    	return $this->template;
+        return $this->template;
     }    
 }
 
